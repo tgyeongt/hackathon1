71b1a10892896eaa9eb999361e9932d1d32b35b2
Taegyeong Park <tgyeongt@gmail.com>
2024-07-26 03:18:20 +0900

    .

--- src/pages/LowerPage.jsx
+++ src/pages/LowerPage.jsx
@@ -8,28 +8,36 @@ import squat from '/src/assets/squat.png';
 import lunge from '/src/assets/lunge.png';
 import calfraise from '/src/assets/calfraise.png';
 
+const exercises = [
+    { name: 'Squat', image: squat },
+    { name: 'Lunge', image: lunge },
+    { name: 'Calf Raise', image: calfraise },
+    // 다른 운동들도 여기에 추가
+  ];
+
 export default function LowerPage() {
     const [liked, setLiked] = useState([false, false, false]);
+    
     const handleLikeClick = (index) => {
         setLiked(prevLiked => {
           const newLiked = [...prevLiked];
           newLiked[index] = !newLiked[index];
           return newLiked;
         });
-      };
+    };
 
     return(
         <TutorialContainer>
-            <ExerciseList>
-                {[squat, lunge, calfraise].map((exercise, index) => (
+         <ExerciseList>
+            {exercises.map((exercise, index) => (
                 <ExerciseCard key={index}>
-                    <img className='cardImage' src={exercise} alt="exercise"/>
+                    <img className='cardImage' src={exercise.image} alt={exercise.name} />
                     <LikeButton onClick={() => handleLikeClick(index)}>
-                    <img src={liked[index] ? fullIcon : emptyIcon} alt="like"/>
+                     <img src={liked[index] ? fullIcon : emptyIcon} alt="like" />
                     </LikeButton>
                 </ExerciseCard>
-                ))}
-            </ExerciseList>
+            ))}
+         </ExerciseList>
         </TutorialContainer>
     )
 }
