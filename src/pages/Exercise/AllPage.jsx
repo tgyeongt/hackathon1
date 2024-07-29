50e0a7f17afb5f7209b0394de7097d20b04bd881
Taegyeong Park <tgyeongt@gmail.com>
2024-07-29 16:34:33 +0900

    .

--- src/pages/Exercise/AllPage.jsx
+++ src/pages/Exercise/AllPage.jsx
@@ -1,13 +1,67 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  TutorialContainer, ExerciseList, ExerciseCard, LikeButton,
-} from '../TutorialStyles';
+import styled, { css, keyframes } from 'styled-components';
+
 import emptyIcon from '/src/assets/emptyheart.png';
 import fullIcon from '/src/assets/fullheart.png';
 import squat from '/src/assets/squat.png';
 import lunge from '/src/assets/lunge.png';
 import pushup from '/src/assets/pushup.png';
+
+const TutorialContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  font-size: 12px;
+`;
+
+const ExerciseList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  gap: 20px;
+  width: 100%;
+`;
+
+const ExerciseCard = styled.div`
+  width: 90%;
+  cursor: pointer;
+  position: relative;
+  margin: 0 auto;
+  align-items: center;
+
+  & .cardImage {
+    width:100%;
+  }
+  
+`;
+
+const LikeButton = styled.button`
+  position: absolute;
+  top: 15px;
+  left: 10px;
+  width: 20px;
+  height: 20px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  img {
+    width: 22px;
+    height: 20px;
+  }
+`;
 
 export default function AllPage() {
     const [liked, setLiked] = useState([false, false, false]);
