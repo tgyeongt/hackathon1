ed2138f05694ab85cabeaef07659fa90976b073d
Taegyeong Park <tgyeongt@gmail.com>
2024-07-31 21:05:42 +0900

    .

--- src/pages/Store/ProductData.js
+++ src/pages/Store/ProductData.js
@@ -1,19 +1,19 @@
 export const productData = [
     {
-        img: '/src/assets/livfit_slim_tshirts.png', // Replace with actual image paths
+        img: '/assets/livfit_slim_tshirts.png',
         text1: 'Livfit Slim T-shirts',
-        text2: '리빗 슬림 티셔츠',
+        text2: '리브핏 슬림 티셔츠',
         text3: '690,000 P',
         text4: '69,000원',
         navigateTo: '/product/livfit-slim-tshirts'
     },
     {
-        img: '/src/assets/black_label_tshirt.png', // Replace with actual image paths
+        img: '/assets/black_label_tshirt.png',
         text1: 'Black Label Signiture T-shirt',
         text2: '블랙 라벨 시그니처 티셔츠',
         text3: '890,000 P',
         text4: '89,000원',
         navigateTo: '/product/black-label-tshirt'
     },
-    // Add more product data as needed
+    // 데이터 추가
 ];
